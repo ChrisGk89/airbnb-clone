44fc30a902ea2e09831d3d69f942385a22127df0
Chris <info@chrisgkalfas.com>
2024-02-23 16:59:21 +0200

    Building navbar pt2

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import Container from "../Container";
 import Logo from "./Logo";
+import Search from "./Search";
 
 const Navbar = () => {
     return (
@@ -17,8 +18,11 @@ const Navbar = () => {
                         items-center
                         gap-3
                         md:gap-0
-                    ">
-                    <Logo />
+                        justify-between
+                        "
+                    >
+                        <Logo />
+                        <Search />
                     </div>
                 </Container>
             </div>
